09cea4f5440aec7ab984dba25d858143e76fa48e
RatedR20 <jcmratedr@gmail.com>
2015-06-02 23:48:27 +0200

    ByWolves
    Web

---
+++ ByWolves/js/fondo.js
@@ -0,0 +1,10 @@
+/**
+ * Created by ByWolves on 14/05/2015.
+ */
+
+
+$(document).ready(function() {
+    var random = Math.round(Math.random()*4);
+    document.body.style.background = "url('./images/fondo/"+random+".jpg') no-repeat fixed";
+    document.body.style.backgroundSize= "cover";
+});
